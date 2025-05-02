f8c102bd7b3fe41943441965bd8c42ec9375a196
Daphne <dsg252@cbsulm35.biohpc.cornell.edu>
2025-05-02 02:38:45 -0400

    colors.R

---
+++ code/colors.R
@@ -0,0 +1,55 @@
+# Abbreviations for snail experimental groups:
+# CF = control female
+# LF = low temperature female
+# HF = high temperature female
+# CM = control male
+# LM = low temperature male
+# HM = high temperature male
+
+
+station_colors <- c(
+  "HF" = "#FF5959",
+  "CF" = "#FFAD20",
+  "LF" = "#FFDE74",
+  "HM" = "#5B5BD1",
+  "CM" = "#2A8ED6",
+  "LM" = "#A3DFFF")
+
+
+temperature_colors <- c(
+  "H" = "#FF5959",
+  "C" = "#FFDE74",
+  "L" = "#2A8ED6")
+
+
+sex_colors <- c(
+  "M" = "#12263A",
+  "F" = "#06BCC1")
+
+phylum_colors <- c(
+  Pseudomonadota = "royalblue",
+  Bacteroidota = "gold", 
+  Bacillota = "deeppink2",
+  Bdellovibrionota = "red1",
+  Acidobacteriota = "navy", 
+  Actinomycetota = "darkslategray2", 
+  Verrucomicrobiota = "darkorchid1",
+  Armatimonadota = "deeppink1",
+  Alphaproteobacteria = "plum2", 
+  Betaproteobacteria = "plum1", 
+  Chloroflexi="black", 
+  "Candidatus Kapabacteria" = "firebrick",
+  Cyanobacteriota = "limegreen",
+  Deltaproteobacteria = "grey", 
+  Desulfobacterota="magenta",
+  Firmicutes = "#3E9B96",
+  Gammaproteobacteria = "greenyellow",
+  "Marinimicrobia (SAR406 clade)" = "yellow",
+  Myxococcota = "#B5D6AA",
+  Planctomycetota = "darkorange", 
+  "SAR324 clade(Marine group B)" = "olivedrab",
+  Proteobacteria_unclassified = "greenyellow",
+  Thermoplasmatota = "green",
+  Thermoproteota = "palevioletred1",
+  Other = "grey")
+
